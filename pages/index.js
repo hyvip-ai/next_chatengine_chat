fffd6af654932cd81cbf9899f87dda5a9c8eeafd
hyvip-ai <rm2932002@gmail.com>
2021-11-27 12:07:22 +0530

    Initial Commit

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,41 @@
-import React from "react";
-
+import React, { useContext } from "react";
+import { UserContext } from "../context";
+import { useRouter } from "next/router";
+import axios from "axios";
 export default function Auth() {
-  return <div className="background">auth</div>;
+  const { userName, setUserName, userSecret, setUserSecret } =
+    useContext(UserContext);
+  return (
+    <div className="background">
+      <div className="auth-container">
+        <form
+          className="auth-form"
+          onSubmit={(e) => {
+            e.preventDefault();
+          }}
+        >
+          <div className="auth-title">Next-Js chat</div>
+          <div className="input-container">
+            <input
+              type="text"
+              className="text-input"
+              placeholder="Username"
+              onChange={(e) => {
+                setUserName(e.target.value);
+              }}
+            />
+
+            <input
+              type="password"
+              className="text-input"
+              placeholder="Password"
+              onChange={(e) => {
+                setUserSecret(e.target.value);
+              }}
+            />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
